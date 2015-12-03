69cde6fc4ee166e030513dfce4620ff812c4949c
Fran Guijarro <franleplant@gmail.com>
2015-12-03 17:45:44 -0300

    a lot of improves

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
 import Color from 'color-js'
-import drawPointFactory from './drawPoint'
 
 let C = [0.279, 0]
-let MAX_ITER = 1000;
 let N = 500
 let M = N;
-let RangeX = 3
+let RangeX = 2
 let RangeY = RangeX
-let BAILOUT = 2
-
 let COLOR_K = 5
+
+let canvas = document.getElementById('canvas');
+canvas.width = N;
+canvas.height = M;
+let ctx = canvas.getContext("2d");
+
+function getInitialState(xCenter, yCenter, RangeX = 2, RangeY = 2, N, M) {
+  return {
+    dx: RangeX / N,
+    dy: RangeY / M,
+    x0: xCenter - (RangeX / 2),
+    y0: yCenter + (RangeY / 2)
+  }
+}
+
 let initialColor = Color({hue: 150, saturation: 0.9, value: 0.9})
 
+function draw() {
+  RangeY = RangeX *= 0.95
+  let initialState =  getInitialState(0.5, 0.174, RangeX, RangeY, N, M)
+  let B = fractal(initialState, initialColor, N, M)
+  let arr = new Uint8ClampedArray(B)
+  let img = new ImageData(arr, N, M)
+  ctx.putImageData(img, 0, 0)
+  window.requestAnimationFrame(draw)
+}
+
+window.requestAnimationFrame(draw)
 
 
-let drawPoint = drawPointFactory(N, M, initialColor, MAX_ITER, COLOR_K)
 
-fractal(drawPoint)
+let BAILOUT = 2
+let MAX_ITER = 360;
+// (initialState) => colorArray B
+// colorArray: [r0,g0,b0,a0, r1,g1,b1,a1, ...]
+function fractal({x0, y0, dx, dy}, initialColor, N, M) {
+  let k, x, y, xx, yy, u, v, pos, col, color
+  let B = []
 
-function fractal(drawPoint) {
-  let k, x, y, xx, yy, u, v
-  let dx = RangeX / N
-  let dy = RangeY / M
 
   for (let j = 0; j < M; j++ ) {
     for (let i = 0; i < N; i++) {
-      x = -(RangeX / 2) + i*dx
-      y =  (RangeY / 2) - j*dy
-      console.log(i,j,x,y)
+      x = x0 + i*dx
+      y = y0 - j*dy
 
       xx = x*x
       yy = y*y
@@ -42,9 +64,19 @@ function fractal(drawPoint) {
         xx = x*x
         yy = y*y
       }
-      drawPoint(i, j, k)
+      pos = 4 * M * j
+      col = 4 * i
+
+      let {red, green, blue, alpha} = initialColor.shiftHue(k * COLOR_K % 360).toRGB()
+      B[pos + col + 0] = red*255
+      B[pos + col + 1] = green*255
+      B[pos + col + 2] = blue*255
+      B[pos + col + 3] = alpha*255
+
     }
   }
+
+  return B
 }
 
 
